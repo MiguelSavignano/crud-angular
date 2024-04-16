033743b61174af6af195f2ff54079230b2d0a819
Miguel Savignano <m.savignano@stuart.com>
2024-04-16 20:40:08 +0200

    Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormControl,  ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'app-example';
   items = ['manzana', 'pera', 'platano'];
+  usuarios: any = []
+  nuevoUsuarioNombre = new FormControl('');
 
   agregarItem(item: string) {
     this.items.push(item)
@@ -19,5 +22,11 @@ export class AppComponent {
 
   quitarItem() {
     this.items.pop()
+  }
+
+  guardarUsuario() {
+    this.usuarios.push({
+      nombre: this.nuevoUsuarioNombre.value || ""
+    })
   }
 }
