34a4de15909886958a61fc2b81272cd72115d047
Miguel Savignano <m.savignano@stuart.com>
2024-04-16 20:20:53 +0200

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'app-example';
+  items = ['manzana', 'pera', 'platano'];
+
+  agregarItem(item: string) {
+    this.items.push(item)
+  }
+
+  quitarItem() {
+    this.items.pop()
+  }
 }
