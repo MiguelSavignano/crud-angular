b492e0b83b0c48730d7568a66b70da9888b1e026
Miguel Savignano <m.savignano@stuart.com>
2024-04-30 20:33:37 +0200

    trabajadores

---
+++ src/app/trabajadores/trabajadores.component.ts
@@ -0,0 +1,23 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-trabajadores',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './trabajadores.component.html',
+  styleUrl: './trabajadores.component.scss',
+})
+export class TrabajadoresComponent {
+  clickEnabled = false;
+  items = [
+    { name: 'manzana', price: 12, a: "A", b: "B", c: "C"},
+    { name: 'pera', price: 10, a: "A", b: "B", c: "C"},
+    { name: 'platano', price: 1, a: "A", b: "B", c: "C"},
+  ];
+
+  onClickChangeTable() {
+    this.clickEnabled = !this.clickEnabled;
+  }
+}
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,32 +1,37 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FormControl,  ReactiveFormsModule } from '@angular/forms';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-users',
   standalone: true,
   imports: [CommonModule, RouterOutlet, ReactiveFormsModule],
   templateUrl: './users.component.html',
-  styleUrl: './users.component.scss'
+  styleUrl: './users.component.scss',
 })
 export class UsersComponent {
   title = 'app-example';
   items = ['manzana', 'pera', 'platano'];
-  usuarios: any = []
+  usuarios: any = [];
   nuevoUsuarioNombre = new FormControl('');
 
+  @Input()
+  set id(id: string) {
+    console.log({id})
+  }
+
   agregarItem(item: string) {
-    this.items.push(item)
+    this.items.push(item);
   }
 
   quitarItem() {
-    this.items.pop()
+    this.items.pop();
   }
 
   guardarUsuario() {
     this.usuarios.push({
-      nombre: this.nuevoUsuarioNombre.value || ""
-    })
+      nombre: this.nuevoUsuarioNombre.value || '',
+    });
   }
 }
